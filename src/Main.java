bac3a82c73b12c49a3b4c42453294ef5889f8d7c
Владимир <vkuznecov1504@mail.ru>
2024-03-17 15:10:09 +0400

    Исправили код, создали класс-родитель Transport Создали интерфейс TransportInterface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        Transport bicycle = new Bicycle("Bicycle", 2);
+        Transport bicycle2 = new Bicycle("Bicycle2", 2);
+
+        Transport car = new Car("Car", 4);
+        Transport car2 = new Car("Car2", 4);
+
+        Transport truck = new Truck("Truck", 8);
+        Transport truck2 = new Truck("Truck2", 8);
+
+        Transport[] transports = {bicycle, bicycle2, car, car2, truck, truck2};
+
+        ServiceStation station = new ServiceStation();
+
+        for (Transport transport : transports) {
+            station.check(transport);
+        }
     }
 }
---
+++ src/ServiceStation.java
@@ -0,0 +1,5 @@
+public class ServiceStation{
+    public void check(TransportInterface transport) {
+        transport.service();
+    }
+}
